25124766c434e8621ac5efa8ef8e3fec1780a41a
ekwaro <dominicnoelchris@gmail.com>
2025-06-16 08:50:19 +0300

    change middlewarefiles

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,17 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
+    {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://dashing-cajeta-d5c85c.netlify.app', // ✅ your Netlify site
+        'http://localhost:3000' // (optional: for local dev)
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      headers: '*',
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
